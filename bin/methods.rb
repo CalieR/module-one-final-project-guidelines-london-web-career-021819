772be25ff972b9c820f103328647255ce2294c2f
github username <github email address>
2019-03-05 20:59:19 +0000

    updated CLI

--- bin/methods.rb
+++ bin/methods.rb
@@ -1,48 +1,52 @@
-require 'pry'
+require_relative '../config/environment'
+prompt = TTY::Prompt.new
+# def greet
+#   puts "Welcome!!"
+#   puts "Please enter your name:"
+#   input = gets.chomp
+#   puts "Hello #{input}!"
+#   input
+# end
 
-def greet
-  puts "Welcome!!"
-  puts "Please enter your name:"
-end
+# TODO : split into greeting and menu methods
 
-def create_user
-  input = gets.chomp
-  puts "Hello #{input}!"
-  Users.create(username: input)
+def menu
+  prompt = TTY::Prompt.new
+  name = prompt.ask('Please enter your username?')
+  search_name = Users.find_or_create_by(username: name)
+  puts "Hello #{name}"
+  choices = [{name: 'View my collection'},
+  {name: 'Add new cards'},
+  {name: 'Delete cards from colection'},
+  {name: 'Exit'}]
+  answer = prompt.select("What would you like to do?", choices)
+  if answer == 'Add new cards'
+    search_name.choose_and_add_card_to_user_deck
+  elsif answer == 'View my collection'
+    search_name.check_collection
+  elsif answer == 'Delete cards from colection'
+    search_name.delete_card
+  elsif answer == 'Exit'
+    puts "Bye bye!!"
+  end
 end
 
 def choose_hero
   puts "Which superhero would you like to read about?"
   input = gets.chomp
-  Cards.find_by(name: input)
+  choice = Cards.find_by(name: input)
+  if !choice
+    puts "Character not found"
+    choose_hero
+  else
+    display_card_details(choice)
+  end
 end
 
-def display_card_details
-  choice = choose_hero
+#TODO : Format desired output
+
+def display_card_details(choice)
   puts choice["name"]
   puts choice["intelligence"]
   puts choice["power"]
 end
-
-def add_card_to_deck?
-  puts "DO you want to add this card to your collection?"
-  puts "enter Y or N"
-  input = gets.chomp
-  if input == "Y"
-    # add to collection
-    # choose_and_add_card_to_user_deck(card)
-  end
-  puts "Do you want to see another card?"
-  puts "enter Y or N"
-  input2 = gets.chomp
-  if input2 == "Y"
-    choose_hero
-  else
-    puts "Thanks for playing"
-  end
-end
-
-
-
-# binding.pry
-0
